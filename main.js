7b4fb0f216dbae2986d5adcdec0c2895dcd87006
Damian <damianbenitez95@gmail.com>
2023-05-28 21:06:18 -0300

    Entrega 2

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
+class miEdad {
+    constructor(edad){
+        this.edad = edad;
+    }
+    
+}
+function edadIngreso(){
+    while(edad = Number || edad < 18){
+    edad = parseInt(prompt("Ingrese su edad "));
+    
+    if(edad >=18){
+        alert("Bienvenido!")
+        return edad
+    }
+    else if(edad < 18){
+        alert("Debes ser mayor de edad");
+    }
+}
+}
+
+class Nombre {
+    constructor(nombre){
+        this.nombre = nombre;
+    }
+}
 function solicitarNombre(){
     let nombre = true
     while(nombre != Number){
@@ -13,56 +38,110 @@ function solicitarNombre(){
 }
 
 
-
-
-
-function edadIngreso(){
-        while(edad = Number || edad < 18){
-        edad = parseInt(prompt("Ingrese su edad "));
-        
-        if(edad >=18){
-            alert("Bienvenido!")
-            return edad
-        }
-        else if(edad < 18){
-            alert("Debes ser mayor de edad");
-        }
-    }
-}
-
-function elegiProducto(){
-    producto = prompt( ` Que esta buscando?
-    1: Whiskys
-    2: Vasos
-    3: Accesorios
-    `)
-
-    switch (producto) {
-        case "1":
-            alert("Elegiste Whiskys")
-            
-            break;
-        case "2":
-            alert("Elegiste Vasos")
-            
-            break;
-        case "3":
-            alert("Elegiste Accesorios")
-                
-            break;
-        default:
-            alert("Ninguna opcion válida")
-            break;
-    }
-
-}
-
-let nombre;
 let edad;
-let producto;
-solicitarNombre()
+let nombre;
 edadIngreso()
-elegiProducto()
+solicitarNombre()
 
 
+class Whisky {
+    constructor(etiquetas, precio){
+        this.etiquetas = etiquetas;
+        this.precio = precio;
+    }
+}
+
+let precios = [
+    {etiquetas:"Red Label", precio:"6000"},
+    {etiquetas:"Black Label", precio:"9000"},
+    {etiquetas:"Double Black Laber", precio:"12000"},
+    {etiquetas:"Green Label", precio:"20000"},
+    {etiquetas:"Blue Label", precio:"60000"},
+]
+
+
+function elegirEtiqueta(){
+    let etiqueta = prompt("Elija la Etiqueta que desea")
+    let precio = parseInt(prompt("Ingrese Precio"))
+
+    const nuevaEtiqueta = new Whisky(etiqueta, precio)
+    arrayCarrito.push(nuevaEtiqueta)
+    alert("Producto añadido al carrito")
+}
+function verCarrito(){
+    arrayCarrito.forEach(producto => {
+        alert(`Eligio ${producto.etiquetas} al precio de ${producto.precio}` )
+    });
+}
+
+
+class Accesorio {
+    constructor(accesorios, precio){
+        this.accesorios = accesorios;
+        this.precio = precio;
+    }
+}
+
+let precios2 = [
+    {accesorios: "Vasos de whisky", precio:"500"},
+    {accesorios: "Apoya Vasos de Goma", precio:"1000"},
+    {accesorios: "Kit Cocetelero Completo", precio: "10000"},
+]
+
+function elegirAccesorio(){
+    let accesorios = prompt("Elija el accesorio que desea")
+    let precio = parseInt(prompt("Ingrese Precio"))
+
+    const nuevoAccesorio = new Accesorio (accesorios, precio)
+    arrayCarrito.push(nuevoAccesorio)
+    alert("Producto añadido al carrito")
+}
+
+function verCarrito(){
+    arrayCarrito.forEach(producto => {
+        alert(`Eligio ${producto.accesorios} al precio de ${producto.precio}` )
+    });
+}
+
+function finalizarCompra(){
+ const total = arrayCarrito.reduce((acc, pre)=> acc + pre.precio, 0)
+ alert(`El total a pagar es de ${total}`)
+}
+
+let arrayCarrito = []
+
+
+let opcion = prompt(
+    ` -Ingrese una opcion:
+     1: Comprar etiqueta
+     2: Comprar accesorio
+     3: Ver carrito
+     4: Ir a pagar
+     5: Salir`
+)
+
+while (opcion != "5") {
+    if(opcion === "1") {
+        elegirEtiqueta(arrayCarrito);
+    }
+    if (opcion === "2"){
+        elegirAccesorio(arrayCarrito)
+    } 
+    if (opcion === "3") {
+        verCarrito();
+    }
+    if (opcion === "4"){
+        finalizarCompra()
+    }
+        
+    opcion = prompt(
+        ` -Vuelva a elegir una opcion:
+         1: Seguir comprando etiquetas
+         2: Seguir comprando accesorios
+         3: Ver carrito
+         4: Ir a pagar
+         5: Salir`
+    );
+}
+alert("Gracias por su compra, vuelva pronto")
 
